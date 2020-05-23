267043bc5521bc30b4e7aaa9bd5bc410e99cf19a
Tom Yaxley <tommitytom@gmail.com>
2020-05-23 14:09:44 +1000

    Fixed event filtering

--- src/scripts/ui/util/component.lua
+++ src/scripts/ui/util/component.lua
@@ -1,10 +1,26 @@
+local EventType = {
+	Lifecycle = 0,
+	Input = 1
+}
+
+local eventTypes = {
+	onKey = EventType.Input,
+	onMouseDown = EventType.Input,
+	onMouseUp = EventType.Input,
+	onDoubleClick = EventType.Input
+}
+
 local function emitComponentEvent(eventName, components, ...)
+	local evType = eventTypes[eventName] or EventType.Lifecycle
+
 	for i = #components, 1, -1 do
 		local component = components[i]
 		local ev = component[eventName]
 		if ev ~= nil then
 			local handled = ev(component, ...)
-			if handled ~= false then return true end
+			if evType == EventType.Input then
+				if handled ~= false then return true end
+			end
 		end
 	end
 
