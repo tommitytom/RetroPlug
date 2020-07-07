cf12c77e75bf362f5481d92d5c193e56d23db69b
Tom Yaxley <tommitytom@gmail.com>
2020-07-07 19:00:22 +1000

    Finished basic micromsg prototype

--- src/retroplug/micromsg2/api2.h
+++ src/retroplug/micromsg2/api2.h
@@ -1,109 +1,162 @@
 #include <tuple>
+#include "micromsg/readerwriterqueue.h"
 
-#define Method(func) decltype(func)
+template <typename T, typename... Methods>
+class Proxy {
+private:
+	struct VoidT {};
+
+	template<typename S>
+	struct StoredCall;
+
+	template <typename R, typename... Args>
+	struct StoredCall<R(T::*)(Args...)> {
+		using ReturnT = typename std::conditional_t<std::is_same<R, void>::value, VoidT, R>;
+
+		size_t callId;
+		R(T::* method)(Args...);
+		std::tuple<Args...> params;
+		//std::function<void(ReturnT&)> retHandler;
+		ReturnT ret;
+	};
+
+	using CallVariant = typename std::variant<StoredCall<Methods>*...>;
+	using CallQueue = typename moodycamel::ReaderWriterQueue<CallVariant>;
+
+	CallQueue _outgoing;
+	CallQueue _incoming;
+	size_t _callId = 0;
+
+public:
+	template <typename R, typename... Args>
+	void call(R(T::*fn)(Args...), Args&&... args) {
+		using StoredCallT = StoredCall<R(T::*)(Args...)>;
+
+		StoredCallT* stored = (StoredCallT*)malloc(sizeof(StoredCallT));
+		*stored = StoredCallT{ _callId++, fn, std::forward_as_tuple(args...) };
+
+		_outgoing.enqueue(stored);
+	}
+
+	template <typename R, typename... Args>
+	void callRet(R(T::*fn)(Args...), std::function<void(int&)> cb, Args&&... args) {
+		using StoredCallT = StoredCall<R(T::*)(Args...)>;
+
+		StoredCallT* stored = (StoredCallT*)malloc(sizeof(StoredCallT));
+		*stored = StoredCallT{ _callId++, fn, std::forward_as_tuple(args...), cb };
+
+		_outgoing.enqueue(stored);
+	}
+
+	template <typename R, typename... Args>
+	R callSync(R(T::* fn)(Args...), Args&&... args) {
+		using StoredCallT = StoredCall<R(T::*)(Args...)>;
+
+		StoredCallT* stored = (StoredCallT*)malloc(sizeof(StoredCallT));
+		*stored = StoredCallT{ fn, std::forward_as_tuple(args...) };
+
+		_outgoing.enqueue(stored);
+	}
+
+	void pull() {
+		CallVariant value;
+		while (_incoming.try_dequeue(value)) {
+			std::visit([this](auto&& arg) {
+				constexpr bool HasReturn = std::is_same_v<decltype(arg->ret), VoidT>;
+				if constexpr (!HasReturn) {
+					// Handle return type
+				}
+
+				free(arg);
+			}, value);
+		}
+	}
+
+	class Listener {
+	private:
+		T* _obj;
+		CallQueue* _incoming;
+		CallQueue* _outgoing;
+
+	public:
+		Listener(CallQueue* incoming, CallQueue* outgoing, T* obj = nullptr) 
+			: _incoming(incoming), _outgoing(outgoing), _obj(obj) {}
+
+		void pull() {
+			CallVariant value;
+			while (_incoming->try_dequeue(value)) {
+				std::visit([this](auto&& arg) {
+					if constexpr (std::is_same_v<decltype(arg->ret), VoidT>) {
+						std::apply(arg->method, std::tuple_cat(std::forward_as_tuple(*_obj), arg->params));
+					} else {
+						arg->ret = std::apply(arg->method, std::tuple_cat(std::forward_as_tuple(*_obj), arg->params));
+					}
+
+					_outgoing->enqueue(arg);
+				}, value);
+			}
+		}
+
+		void setObj(T& obj) {
+			_obj = &obj;
+		}
+
+		typename T& getObj() {
+			return *_obj;
+		}
+
+		typename const T& getObj() const {
+			return *_obj;
+		}
+	};
+
+	Listener getListener() {
+		return Listener(&_outgoing, &_incoming);
+	}
+
+	Listener getListener(T& obj) {
+		return Listener(&_outgoing, &_incoming, &obj);
+	}
+};
 
 class Foo {
+private:
+	std::string _text;
+
 public:
 	void bar(int v, float v2) {
 		std::cout << "BAR! " << v << " " << v2 << std::endl;
+		_text = std::to_string(v);
 	}
 
-	int baz() { return 1337; }
-};
-
-using FuncT = decltype(&Foo::bar);
-
-// https://stackoverflow.com/questions/28509273/get-types-of-c-function-parameters
-
-#include <iostream>
-
-template<typename T, typename F>
-void calltest3(T&& t, F&& f)
-{
-	f(std::forward<T>(t));
-}
-
-struct StoredCallBase {
-};
-
-template <typename T, typename R, typename... Args>
-struct StoredCall {
-	R(T::*method)(Args...);
-	std::tuple<Args...> data;
-};
-
-template <typename T, typename R, typename... Args>
-struct StoredCallSplit {
-	//using type = StoredCall
-};
-
-template <typename T, typename Method1, typename Method2>
-class ProxyBase {
-private:
-	//std::vector<std::variant<StoredCall>> _queue;
-	std::variant<StoredCall<T, void, int, float>, StoredCall<T, int>> _data;
-
-public:
-	T* _obj = nullptr;
-
-	template <typename R, typename... Args>
-	std::tuple<Args...> call(R(T::*fn)(Args...), Args&&... args) {
-		_data = StoredCall<T, R, Args...> { fn, std::tuple<Args...>(args...) };
-		return std::tuple<Args...>(args...);
-	}
-
-	void callDeferred() {
-		std::visit([this](auto&& arg) {
-			std::apply(arg.method, std::tuple_cat(std::make_tuple(*_obj), arg.data));
-		}, _data);
+	int baz() {
+		std::cout << _text << std::endl;
+		return 1337;
 	}
 };
 
-template <typename T>
-class ListenerBase {
-private:
-	T* _obj = nullptr;
-
-public:
-	ListenerBase(T& obj): _obj(&obj) {}
-
-	template <typename R, typename... Args>
-	void call(R(T::*fn)(Args...), std::tuple<Args...> params) {
-		std::apply(fn, std::tuple_cat(std::make_tuple(*_obj), params));
-	}
-};
-
-template <typename T, typename... Methods>
-struct ClassWrap {
-	
-};
-
-using FooMethods = ClassWrap<Foo, Method(&Foo::bar), Method(&Foo::baz)>;
-
-
+using FooProxy = Proxy<
+	Foo, 
+	decltype(&Foo::bar), 
+	decltype(&Foo::baz)
+>;
 
 static void func() {
 	Foo f;
-
-	//std::variant<FooMethods::type> varTest;
 	
-	ProxyBase<Foo, Method(&Foo::bar), Method(&Foo::baz)> emitter;
-	emitter._obj = &f;
+	FooProxy emitter;
+	FooProxy::Listener listener = emitter.getListener(f);
 
-	auto params = emitter.call(&Foo::bar, 10, 3.14f);
-	emitter.callDeferred();
-	
-	//ListenerBase<Foo> listener(f);
-	//listener.call(&Foo::bar, params);
+	emitter.call(&Foo::bar, 10, 3.14f);
+	emitter.call(&Foo::bar, 1337, 3.14f);
+	emitter.call(&Foo::baz);
 
-	//std::apply(&Foo::bar, std::tuple_cat(std::make_tuple(f), params));
+	/*emitter.callRet(&Foo::baz, [](int& v) {
 
-	//auto f2 = std::bind(&Foo::bar, f);
-	//std::apply(f2, t);
-	
-	//calltest3(f2, 20);
-	
-	//auto args2 = calltest2<Foo, &Foo::bar>(&f);
+	});*/
 
-	//some_function(args_t<decltype(testFunc)>{});
+	listener.pull();
+	emitter.pull();
+
+	std::cout << std::endl;
 }
