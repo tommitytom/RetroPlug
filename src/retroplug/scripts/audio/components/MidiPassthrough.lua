734d659800c226d1bf453909fe4076b03ac602ad
Tom Yaxley <tommitytom@gmail.com>
2020-12-06 10:47:01 +1100

    Fix midi passthrough

--- src/retroplug/scripts/audio/components/MidiPassthrough.lua
+++ src/retroplug/scripts/audio/components/MidiPassthrough.lua
@@ -6,10 +6,10 @@ end
 function MidiPassthrough.onTransportChanged()
 end
 
-function MidiPassthrough.onMidi(message)
-    self:system():sendSerialByte(message.offset, message.status, 8)
-    self:system():sendSerialByte(message.offset, message.data1, 8)
-    self:system():sendSerialByte(message.offset, message.data2, 8)
+function MidiPassthrough.onMidi(system, message)
+    system:sendSerialByte(message.offset, message.status, 8)
+    system:sendSerialByte(message.offset, message.data1, 8)
+    system:sendSerialByte(message.offset, message.data2, 8)
 end
 
 return MidiPassthrough
