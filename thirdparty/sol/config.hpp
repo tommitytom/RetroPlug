d895f9f99ed9ec15515b6531ef2d41a060c15894
Tom Yaxley <tommitytom@gmail.com>
2021-03-23 22:32:01 +1100

    Downgrade sol

--- thirdparty/sol/config.hpp
+++ thirdparty/sol/config.hpp
@@ -20,8 +20,8 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // This file was generated with a script.
-// Generated 2020-11-19 21:39:27.536445 UTC
-// This header was generated with sol v3.2.3 (revision 62804667)
+// Generated 2020-10-03 21:34:25.034794 UTC
+// This header was generated with sol v3.2.1 (revision 48eea7b5)
 // https://github.com/ThePhD/sol2
 
 #ifndef SOL_SINGLE_CONFIG_HPP
