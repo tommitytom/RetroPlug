efc43b2dd398cf4864f48b63441a67f17324a80b
Tom Yaxley <tommitytom@gmail.com>
2022-01-15 10:48:21 +1100

    Update deploy scripts

--- deploy.bat
+++ deploy.bat
@@ -9,13 +9,13 @@ msbuild build/vs2019/ScriptCompiler.vcxproj /property:Configuration=Release /pro
 build\vs2019\bin\x64\Release\ScriptCompiler.exe src\compiler.config.lua
 premake5 vs2019
 
-msbuild build/vs2019/RetroPlug.sln /property:Configuration=Release /property:Platform=x64 /m
+msbuild build/vs2019/RetroPlugAll.sln /property:Configuration=Release /property:Platform=x64 /m
 
 copy build\vs2019\bin\x64\Release\RetroPlug_app_x64.exe build\deploy\RetroPlug-%RETROPLUG_VERSION%.exe
-copy build\vs2019\bin\x64\Release\RetroPlug_vst2_x64.dll "build\deploy\RetroPlug %RETROPLUG_VERSION% (64bit).dll"
+REM copy build\vs2019\bin\x64\Release\RetroPlug_vst2_x64.dll "build\deploy\RetroPlug %RETROPLUG_VERSION% (64bit).dll"
 
 cd build\deploy
 7z a RetroPlug_standalone-win64-%RETROPLUG_VERSION%.zip RetroPlug-%RETROPLUG_VERSION%.exe
-7z a RetroPlug_vst2-win64-%RETROPLUG_VERSION%.zip "RetroPlug %RETROPLUG_VERSION% (64bit).dll"
+REM 7z a RetroPlug_vst2-win64-%RETROPLUG_VERSION%.zip "RetroPlug %RETROPLUG_VERSION% (64bit).dll"
 
 cd ..\..
