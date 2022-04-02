2e138c7aef40fcfa0e65a66f55eec8968c82c42e
Tom Yaxley <tommitytom@gmail.com>
2022-04-02 18:05:40 +1100

    CI

--- deploy.bat
+++ deploy.bat
@@ -7,10 +7,13 @@ mkdir build\deploy
 premake5 vs2019
 msbuild build/vs2019/ScriptCompiler.vcxproj /property:Configuration=Release /property:Platform=x64 /m
 build\vs2019\bin\x64\Release\ScriptCompiler.exe src\compiler.config.lua
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 premake5 vs2019
 
 REM msbuild build/vs2019/RetroPlugAll.sln /property:Configuration=Release /property:Platform=x64 /m
 msbuild build/vs2019/RetroPlugApp.vcxproj /property:Configuration=Release /property:Platform=x64 /m
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 copy build\vs2019\bin\x64\Release\RetroPlug_app_x64.exe build\deploy\RetroPlug-%RETROPLUG_VERSION%.exe
 REM copy build\vs2019\bin\x64\Release\RetroPlug_vst2_x64.dll "build\deploy\RetroPlug %RETROPLUG_VERSION% (64bit).dll"
