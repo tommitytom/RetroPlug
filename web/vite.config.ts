509a0eb07ea9ddf3b961b9752bd15c229ef2c773
tommitytom <git@tommitytom.co.uk>
2025-09-10 20:58:35 +1000

    Sentry

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	server: {
 		headers: {
-			"Cross-Origin-Embedder-Policy": "require-corp",
-			"Cross-Origin-Opener-Policy": "same-origin",
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+			'Cross-Origin-Opener-Policy': 'same-origin',
 		},
 	},
-	plugins: [tailwindcss(), react()],
+
+	plugins: [
+		tailwindcss(),
+		react(),
+		sentryVitePlugin({
+			org: 'retroplug',
+			project: 'retroplugweb',
+		}),
+	],
+
 	resolve: {
 		preserveSymlinks: true,
 	},
+
 	worker: {
 		format: 'es',
+	},
+
+	build: {
+		sourcemap: true,
 	},
 });
